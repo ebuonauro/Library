e3d42bb1f2cbd668a4fa55bc0183ade211b303d8
Emily Buonauro <contact@emilyalleen.com>
2021-02-02 20:15:07 -0800

    Fixed webpack config, getting ready to use library as a component dumping ground

--- src/index.js
+++ src/index.js
@@ -18,22 +18,11 @@ import './components/sticky-navigation/helpers/logic.js';
 import { navHelper } from './components/sticky-navigation/helpers/source.js';
 import Nav from './components/sticky-navigation/config/data.json';
 function componentStickyNav() {
-  const user = {
-    "data": Nav.navigation['user-name'],
-    "elem": document.querySelector('.user__avatar')
-  },
-  navLinks = {
+  const navLinks = {
     "data": Nav.navigation['nav']['links'],
     "elem": document.querySelector('.nav-links__container')
   }
-  user.elem.innerHTML = user.data;
   navHelper(navLinks.data, navLinks.elem);
 
-  if (Nav.navigation['user-rank'] === 'admin') {
-    document.querySelector('.cp-links').innerHTML = '<li class="cp-links__admin admin"><a href="http://lintskins.jcink.net/admin.php" target="_blank">Admin CP</a></li>\
-    <li class="cp-links__mod mod"><a href="http://lintskins.jcink.net/mod.php" target="_blank">Mod CP</a></li>'
-  } else if (Nav.navigation.user-rank === 'mod') {
-    document.querySelector('.cp-links').innerHTML = '<li class="cp-links__mod mod"><a href="http://lintskins.jcink.net/mod.php" target="_blank">Mod CP</a></li>'
-  }
 }
 componentStickyNav();
